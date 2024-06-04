f5fe69873e5f4d65895bac9e4a7deec5f11b810f
mariamoraan <mariamoranluaces@gmail.com>
2024-06-04 23:02:10 +0200

    chore: create styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./core/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <p>Holaa</p>
+  </React.StrictMode>
 );
